6f51084a13fd5c854bb7239246789bdd7e255596
rodrigoRolim <rodrigorolimveras92@gmail.com>
2026-01-26 10:26:09 -0300

    add datetime inputs to form

--- components/transport-request/TravelDataStep.tsx
+++ components/transport-request/TravelDataStep.tsx
@@ -4,11 +4,13 @@
 // Date and time return
 // number of passengers
 // vehicle type will be matched accordingly to number of passengers
+'use client';
 
-import { Clock, MapPin, Timer, Users } from "lucide-react";
+import { Clock, MapPin, Users } from "lucide-react";
 import InputText from "../InputText";
 import InputTime from "../InputTime";
 import InputNumber from "../InputNumber";
+import DatePicker from "../DatePicker";
 
 export default function TravelDataStep() {
   return (
@@ -17,13 +19,19 @@ export default function TravelDataStep() {
         <h2 className="text-xl font-semibold text-slate-900">
           Dados da viagem
         </h2>
-        <p>Detalhes do trajeto</p>
+        <p className="text-slate-500">Detalhes do trajeto</p>
       </div>
       <div className="flex flex-col gap-6 mb-4">
         <InputText label="Local de embarque *" icon={MapPin} placeholder="Endereço completo de embarque"/>
-        <InputTime label="Horário de saída *" icon={Clock} placeholder="HH:MM"/>
+        <div className="flex flex-col xs:flex-row gap-4">
+          <DatePicker label="Data de saída" className="flex-1"/>
+          <InputTime label="Horário de saída *" icon={Clock} placeholder="HH:MM" className="flex-1"/>
+        </div>
         <InputText label="Destino *" icon={MapPin} placeholder="Endereço completo de destino"/>
-        <InputTime label="Horário de retorno *" icon={Clock} placeholder="HH:MM"/>
+        <div className="flex gap-4">
+          <DatePicker label="Data de retorno" className="flex-1"/>
+          <InputTime label="Horário de retorno *" icon={Clock} placeholder="HH:MM" className="flex-1"/>
+        </div>
         <InputNumber label="Número de passageiros *" icon={Users} value={0} placeholder="" onChange={() => {}}/>
       </div>
     </form>
