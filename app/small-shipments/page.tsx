289f3dc678267a22f6d01214f0b5a6f088f64197
rodrigoRolim <rodrigorolimveras92@gmail.com>
2026-02-13 13:06:20 -0300

    refator: shrink the font size

--- app/small-shipments/page.tsx
+++ app/small-shipments/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import AdSmallShipmentCard from "@/components/small-shipment/AdSmallShipmentCard";
 import DeliveryTypeFilters, { DeliveryType } from "@/components/small-shipment/DeliveryTypeFilters";
 import VehicleTypeFilters, { VehicleType } from "@/components/small-shipment/VehicleTypeFilters";
+import { ChevronLeft } from "lucide-react";
 import { useState } from "react";
 
 export default function SmallShiptments() {
@@ -9,24 +11,32 @@ export default function SmallShiptments() {
   const [deliveryType, setDeliveryType] = useState<DeliveryType>('all')
 
   return (
-    <div className="max-w-7xl mx-auto p-4 md:p-6">
-      <header className="flex flex-col">
-        <h1 className="text-2xl font-bold text-gray-900 mb-2">Encomendas & Entregas</h1>
-        <p className="text-gray-600">Serviços de entrega rápida e segura</p>
+    <div className="max-w-7xl mx-auto p-4 md:p-6 bg-transparent">
+      <header className="flex gap-2">
+        <button className="flex items-start justify-start w-fit h-fit">
+          <ChevronLeft className="h-6"/>
+        </button>
+        <div className="flex flex-col justify-start">
+          <h1 className="text-lg leading-5 font-bold text-gray-900 mb-2">Encomendas & Entregas</h1>
+          <p className="text-sm text-gray-600">Serviços de entrega rápida e segura</p>
+        </div>
       </header>
-      <div className="flex flex-col gap-1 mt-4">
-        <h2 className="text-sm text-gray-600 font-medium">Filtrar por veículo:</h2>
+      <div className="flex flex-col gap-0.5 mt-6">
+        <h2 className="text-xs text-gray-600 font-medium">Filtrar por veículo:</h2>
         <VehicleTypeFilters
           value={vehicleType}
           onChange={setVehicleType}
         />
       </div>
-      <div className="flex flex-col gap-1 mt-2">
-        <h2 className="text-sm text-gray-600 font-medium">Filtrar por tipo de encomenda:</h2>
+      <div className="flex flex-col gap-0.5 mt-2">
+        <h2 className="text-xs text-gray-600 font-medium">Filtrar por tipo de encomenda:</h2>
         <DeliveryTypeFilters 
           value={deliveryType}
           onChange={setDeliveryType}
         />
+      </div>
+      <div className="grid grid-cols-1 mt-6">
+        <AdSmallShipmentCard />
       </div>
     </div>
   )
