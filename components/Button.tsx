38fe82c8b59f827b4ba789f1bb4ae3b88142d004
rodrigoRolim <rodrigorolimveras92@gmail.com>
2026-02-24 18:22:01 -0300

    add loading state into button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,24 +1,28 @@
 import { LucideIcon } from "lucide-react";
+import AnimatedSpin from "./AnimatedSpin";
 
-interface Props {
+type Props = {
   label: string;
-  icon: LucideIcon;
+  icon?: LucideIcon;
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
   type?: 'button' | 'submit'
   variant?: 'primary' | 'secondary'
-}
-export default function Button({ label, icon: Icon, onClick, type = 'button', variant = 'primary' }: Props) {
+  loading?: boolean;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+export default function Button({ label, icon: Icon, onClick, type = 'button', variant = 'primary', loading = false, ...props }: Props) {
   const variantsMap = {
     primary: 'from-orange-500 to-orange-600 hover:from-orange-600 hover:to-orange-700 shadow-orange-500/25',
     secondary: 'from-slate-600 to-slate-800 hover:from-slate-600 hover:to-slate-700 shadow-slate-500/25'
   }
   return (
     <button
+     {...props}
       type={type}
-      className={`inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:bg-primary/90 py-2 w-full md:w-auto h-14 px-8 rounded-xl bg-linear-to-r ${variantsMap[variant]} text-white font-semibold shadow-lg`}
+      className={`inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:bg-primary/90 py-2 w-full h-14 px-8 rounded-xl bg-linear-to-r ${variantsMap[variant]} text-white font-semibold shadow-lg ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
       onClick={onClick}
     >
-      {Icon && <Icon className="size-5 text-white"/>}
+      {Icon && !loading && <Icon className="size-5 text-white"/>}
+      {loading && <AnimatedSpin />}
       {label}
     </button>
   )
